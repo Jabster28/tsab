6d33b1ee5928db7d36cdfa304dc9a3ea1dbedc00
Jabster28 <justynboyer@gmail.com>
2020-05-24 02:18:42 +0100

    build(firebase): add firebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -24,4 +25,14 @@ module.exports = {
     path: path.join(__dirname, "dist"),
   },
   devtool: "sourcemap",
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public/",
+          to: "./",
+        },
+      ],
+    }),
+  ],
 };
